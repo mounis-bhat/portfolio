c9b6baefd1b4a4aca4c806d88b901f2d7aa2a976
Mounis Bhat <mounisbhat@gmail.com>
2021-12-27 19:53:57 +0530

    Add Favicon

--- components/layouts/Main.jsx
+++ components/layouts/Main.jsx
@@ -9,6 +9,7 @@ const Main = ({ children, router }) => {
       <Head>
         <title>Mounis Bhat - Homepage</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/logo.svg" />
       </Head>
 
       <Navbar path={router.asPath} />
