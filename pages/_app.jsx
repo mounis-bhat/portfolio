ccc0568e32e8d7e3acbf54429ccdb82a2737571b
Mounis Bhat <mounisbhat@gmail.com>
2021-12-27 15:14:02 +0530

    Replace default font with "M PLUS Rounded 1c"

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react"
+
 import Main from "../components/layouts/Main"
 import theme from "../libs/theme"
+import Fonts from "../components/fonts"
 
 const App = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Main router={router}>
         <Component {...pageProps} key={router.route} />
       </Main>
