a94f7ecf0c1ee845aafd3ea8fe98816e57109959
Mounis Bhat <mounisbhat@gmail.com>
2021-12-30 14:58:43 +0530

    Update Works Section 1. Change alignment of heading 2. Update contents of Noqod Project 3. Add Portfolio to works section 4. Update thumbnails of Portfolio and Noqod 5. Remove screenshots from README.md

--- pages/works.jsx
+++ pages/works.jsx
@@ -5,19 +5,29 @@ import Section from "../components/Section"
 import { WorkGridItem } from "../components/GridItem"
 
 import thumbnailNoqod from "../public/images/works/Noqod.png"
+import thumbnailPortofolio from "../public/images/works/Logo.png"
 
 const Works = () => {
   return (
     <Article>
       <Container>
-        <Heading as="h3" fontSize={20} mb={10} mt={10}>
-          Works
+        <Heading as="h3" fontSize={30} mb={10} mt={10} align="center">
+          Projects
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={16}>
           <Section delay={0.1}>
             <WorkGridItem id="noqod" title="Noqod" thumbnail={thumbnailNoqod}>
               Noqod is a service provider instituted with an impetus of
               revolutionalising digital payment process in MENA
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.2}>
+            <WorkGridItem
+              id="portfolio"
+              title="Personal Portfolio"
+              thumbnail={thumbnailPortofolio}
+            >
+              An application that gives you a glimpse of my work and skills
             </WorkGridItem>
           </Section>
         </SimpleGrid>
